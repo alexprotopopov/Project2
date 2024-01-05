da968eb99a05c176275195cf25e1b698d2c4089f
alexprotopopov <37345265+alexprotopopov@users.noreply.github.com>
2024-01-05 20:28:41 +0300

    Completed the task

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private  final static String DB_DRIVER ="com.mysql.cj.jdbc.Driver";
+    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        Connection connection= null;
+
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); {
+            //    System.out.println("We are connected!");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("there is no connection... Exception!");
+        }
+        return connection;
+    }
 }
+
+
